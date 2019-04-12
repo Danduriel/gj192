bb15f8f33fefdb241eba07f0c6737cac1436526f
Alex Abeler <meckzankzwerg@yahoo.de>
2019-04-12 19:51:28 +0200

    initial

---
+++ html/src/gj/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package gj.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import gj.game.Orchestrator;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Orchestrator();
+        }
+}

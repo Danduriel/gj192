f0a6fafa151e0e2ef9a6e854a64428533ed52400
Alex Abeler <meckzankzwerg@yahoo.de>
2019-04-13 23:26:26 +0200

    update stone friction

--- core/src/gj/game/gjBodyFactory.java
+++ core/src/gj/game/gjBodyFactory.java
@@ -130,8 +130,9 @@ public class gjBodyFactory {
                 break;
             case STONE:
                 fixtureDef.density = 1f;
-                fixtureDef.friction = 0.01f;
-                fixtureDef.restitution = 0.1f;
+                fixtureDef.friction = 0.5f;
+                fixtureDef.restitution = 0f;
+                break;
             default:
                 fixtureDef.density = 7f;
                 fixtureDef.friction = 0.5f;
